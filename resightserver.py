1b2518035c95db0621b9ffba85843d129e4e3480
young <siosio3103@gmail.com>
2017-04-05 15:00:13 +0900

    flask api test

--- resightserver.py
+++ resightserver.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
+#TODO 파이어베이스 데이터 가져오기 + 안드로이드에 데이터주기.
+
+@app.route('/resightapp', methods = ['GET','POST'])
+def getAndroidAppLink():
+    n
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
